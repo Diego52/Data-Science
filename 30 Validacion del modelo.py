e4f52610f97fa8f29c30d09e1080ab7a6c419007
Soriano Morales <d.soriano.morales@accenture.com>
2018-10-02 15:48:20 -0500

    Regresion con sklearn

--- 30 Validacion del modelo.py
+++ 30 Validacion del modelo.py
@@ -1,7 +1,23 @@
 import pandas as pd 
 import numpy as np 
 import matplotlib.pyplot as plt 
+import statsmodels.formula.api as smf
 #Dividir el dataset en conjunto de entrenamiento y de testing
 
 data = pd.read_csv("python-ml-course-master/datasets/ads/Advertising.csv")
 
+a = np.random.randn(len(data))
+check = (a<0.8)
+training = data[check]
+testing = data[~check]
+
+lm = smf.ols(formula = "Sales~TV+Radio", data= training).fit()
+print(lm.summary())
+
+#Validacion del modelo
+sales_pred = lm.predict(testing)
+SSD = sum((testing["Sales"]-sales_pred)**2)
+RSE = np.sqrt(SSD / (len(testing)-3))
+sales_mean = np.mean(testing["Sales"])
+error = RSE / sales_mean
+print(error)
---
+++ 31 Regresion lineal con Scikit learn.py
@@ -0,0 +1,22 @@
+from sklearn.feature_selection import RFE
+from sklearn.svm import SVR 
+import pandas as pd 
+from sklearn.linear_model import LinearRegression
+import numpy as np 
+
+data = pd.read_csv("python-ml-course-master/datasets/ads/Advertising.csv")
+features_cols = ["TV", "Radio", "Newspaper"]
+x= data[features_cols]
+y = data["Sales"]
+estimator = SVR(kernel = "linear") #crea un modelo lineal
+selector = RFE(estimator, 2, step=1) #Le pedimos que deje el modelo en 2 variables predictoras Recursive Feature Elimination
+selector = selector.fit(x,y)
+print(selector.support_)
+print(selector.ranking_)
+
+X_pred = x[["TV","Radio"]] 
+lm = LinearRegression() #Crea el modelo de regresion lineal
+lm.fit(X_pred, y) #Ajusta el modelo a nuestros datos
+print(lm.intercept_) #Alpha
+print(lm.coef_) #Bethas 
+print(lm.score(X_pred, y)) #R2
